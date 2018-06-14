454c5fddce9e6265a28682bf059219751084f278
Jordan Krueger <jkrueger702@gmail.com>
2018-06-14 05:51:13 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,20 @@
 class Person
-
+ 
   def initialize(name)
-@name= person_name
+    @name = name
+    @job  = job
+  end
+ 
+  def name
+    @name
   end
 
-  def name=
-
+  def job
+    @job
   end
-
+ 
+  def name=(new_name)
+    @name = new_name
+  end
+ 
 end
