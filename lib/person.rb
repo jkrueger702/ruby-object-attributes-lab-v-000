732384c54e8fa4834f94c1a8623053e383b9d268
Jordan Krueger <jkrueger702@gmail.com>
2018-06-14 05:27:24 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,7 +3,7 @@ class Person
   def initialize(name)
       @name = name
     end
-    
+
   def name
   @name
   end
@@ -11,5 +11,5 @@ end
 
   
 
-jack = Person.new
-jack.name = "Jack"
+jack = Person.new("Jack")
+
